d6bac839f916bd3303c814be2a6664e9a74629ea
Anders Hafreager <anderhaf@1x-193-157-210-83.uio.no>
2016-04-12 19:01:19 +0200

    Updated main

--- main.cpp
+++ main.cpp
@@ -4,42 +4,19 @@
 
 using namespace std;
 
-void runSerial(int max) {
-    for(unsigned int i=256; i<=max; i+=128) {
-        WaveSolver solver(i);
-        unsigned int N = 5;
+double run(int gridSize, int timesteps, bool serial) {
+    WaveSolver solver(gridSize);
+    unsigned int N = timesteps;
 
-        struct timeval start, end;
-        gettimeofday(&start, NULL);
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
 
-        for(unsigned int j=0; j<N; j++) {
-            solver.step(true);
-        }
-        gettimeofday(&end, NULL);
-        double timeElapsed = ((end.tv_sec  - start.tv_sec) * 1000000u +
-                                     end.tv_usec - start.tv_usec) / 1.e6;
-        double gflops = 9.0*solver.gridSize()*solver.gridSize()*N / timeElapsed*1e-9;
-        cout << "SERIAL N=" << i << " gflops: " << gflops << " after " << timeElapsed << " seconds." << endl;
+    for(unsigned int j=0; j<N; j++) {
+        solver.step(serial);
     }
-}
-
-void runISPC(int max) {
-    for(unsigned int i=256; i<=max; i+=128) {
-        WaveSolver solver(i);
-        unsigned int N = 5;
-
-        struct timeval start, end;
-        gettimeofday(&start, NULL);
-
-        for(unsigned int j=0; j<N; j++) {
-            solver.step(false);
-        }
-        gettimeofday(&end, NULL);
-        double timeElapsed = ((end.tv_sec  - start.tv_sec) * 1000000u +
-                                     end.tv_usec - start.tv_usec) / 1.e6;
-        double gflops = 9.0*solver.gridSize()*solver.gridSize()*N / timeElapsed*1e-9;
-        cout << "ISPC N=" << i << " gflops: " << gflops << " after " << timeElapsed << " seconds." << endl;
-    }
+    gettimeofday(&end, NULL);
+    double timeElapsed = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+    return solver.gridSize()*solver.gridSize()*N/timeElapsed*1e-6;
 }
 
 int main(int args, char **argv)
@@ -47,8 +24,11 @@ int main(int args, char **argv)
     int maxGridSize = 1024;
     if(args>1) maxGridSize = atoi(argv[1]);
 
-    // runSerial(maxGridSize);
-    runISPC(maxGridSize);
+    for(int gridSize=128; gridSize<=maxGridSize; gridSize += 128) {
+        double speedSerial = run(gridSize, 5, true);
+        double speedISPC = run(gridSize, 5, false);
+        cout << "N=" << gridSize << " speed (million grid points per second): " << speedSerial << " (SERIAL) vs " << speedISPC << " (ISPC), speedup: " << speedISPC/speedSerial << endl;
+    }
     return 0;
 }
 
